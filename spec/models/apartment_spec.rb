312a945cc0634c42b35d0978189121161f540e07
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-26 17:45:19 -0400

    apartment rspec tests pass

---
+++ spec/models/apartment_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../spec_helper.rb'
+
+describe Apartment do
+  before :each do
+    @ga = Apartment.new(name: "GA Speakeasy", street: '10 E 21st Street', apt: '4', zipcode: 10010)
+    @clinton = Apartment.new(name: "Jason's Sweet Pad", street: '40 Clinton Street', apt: '11B', zipcode: 11201)
+  end
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :street }
+  it { should validate_presence_of :apt }
+  it { should validate_presence_of :zipcode }
+  it { should validate_numericality_of :zipcode }
+  it { should ensure_length_of(:zipcode).is_equal_to(5) }
+
+  it { have_many :users }
+  it { have_many :chores }
+
+end
