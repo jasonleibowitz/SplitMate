aed7648bcb63bc07cac42e5001abb915d3814687
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-26 17:45:09 -0400

    apartment crud

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,42 @@
 class ApartmentsController < ApplicationController
 
+  def index
+    @apartments = Apartment.all
+  end
+
+  def show
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def new
+    @apartment = Apartment.new
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+    @apartment.save
+    redirect_to @apartment
+  end
+
+  def edit
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def update
+    @apartment = Apartment.find(params[:id])
+    @apartment.update(apartment_params)
+    redirect_to @apartment
+  end
+
+  def destroy
+    @apartment = Apartment.find(params[:id])
+    @apartment.destroy
+    redirect_to root_path
+  end
+
+  private
+  def apartment_params
+    return params.require(:apartment).permit(:name, :street, :apt, :zipcode)
+  end
+
 end
--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,4 +3,8 @@ class Apartment < ActiveRecord::Base
   has_many :users
   has_many :chores
 
+  validates :name, :street, :apt, :zipcode, presence: true
+  validates :zipcode, numericality: { only_integer: true }
+  validates :zipcode, length: { is: 5 }
+
 end
