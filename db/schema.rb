b16fb738d9ebc7e891dbad9feef40761b3bd418d
verner-dz <verner.dz@gmail.com>
2014-04-26 16:59:12 -0400

    updated roommate_id to user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426161343) do
+ActiveRecord::Schema.define(version: 20140426203552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20140426161343) do
     t.integer  "points_value"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "roommate_id"
+    t.integer  "user_id"
     t.integer  "apartment_id"
     t.date     "due_date"
   end
