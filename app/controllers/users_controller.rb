90402b47f317b1f37245594ac8e0f551bc0a0a43
verner-dz <verner.dz@gmail.com>
2014-04-26 16:58:43 -0400

    set up user show new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,30 @@
 class UsersController < ApplicationController
 
-  before_action :require_authentication
+  before_action :require_authentication, only: [:index, :show, :edit, :update, :destroy]
 
   def index
 
   end
 
   def show
+    @user = User.find(params[:id])
 
   end
 
   def new
-
+    @user = User.new
   end
 
-  def update
+  def create
+    @user = User.new(user_params)
+    @user.points_balance = 0
+    @user.points_lifetime = 0
+    @user.completed_week_points = 0
+    @user.total_week_points = 0
+    @user.admin = false
+    @user.save
 
+    redirect_to @user
   end
 
   def edit
@@ -32,7 +41,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    return params.requier(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    return params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :password, length: { minimum: 5 }
+
 end
