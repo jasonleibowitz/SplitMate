bccf45ad9b30487da5405056e45fe82379dfbd62
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-26 18:52:47 -0400

    validations only happen on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
-  validates :password, length: { minimum: 5 }
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true, :on => :create
+  validates :password, length: { minimum: 5 }, :on => :create
 
 end
