a71c0da033ea48c1b53f47d996583c084ba4eb8c
verner-dz <verner.dz@gmail.com>
2014-04-27 18:50:37 -0400

    routes updated for assignchore/:id

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ SplitmateApp::Application.routes.draw do
   resources :apartments do
     resources :chores, shallow: true
   end
+
   get '/login' => 'sessions#new'
   get '/logout' => 'sessions#destroy'
   post '/sessions' => 'sessions#create'
   get '/apartments/:id/addroommates' => 'apartments#add_roommates'
   post '/saveroommate' => 'users#save_roommate'
+  get '/assignchore/:id/' => 'chores#assign_chore'
 
   root to: 'users#index'
 
