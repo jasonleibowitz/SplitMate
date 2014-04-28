0f90d0c769e2d4fd9c24571398c93e5366e1487d
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-28 12:18:41 -0400

    resolved errors after merging complete chore into method

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -5,7 +5,9 @@ class Chore < ActiveRecord::Base
   has_many :chore_histories
 
 	def complete_chore(comments)
-		# Give user points for completing chore
+		@user = self.user
+
+    # Give user points for completing chore
 		self.user.update_points(self.points_value)
 		self.user.save!
 
@@ -20,8 +22,8 @@ class Chore < ActiveRecord::Base
     @chore_history.chore = self
     @chore_history.name = self.name
     @chore_history.points_value = self.points_value
-    @chore_history.user = self.user
-    @chore_history.apartment = self.user.apartment
+    @chore_history.user = @user
+    @chore_history.apartment = @user.apartment
     @chore_history.save!
   end
 
