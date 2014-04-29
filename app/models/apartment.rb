c47139fd25dbfbdb9442753ffbceb1c1801b2ead
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-29 10:45:32 -0400

    users do not have to provide apt number when creating an apartment because some people live in houses, those lucky dogs.

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -5,7 +5,7 @@ class Apartment < ActiveRecord::Base
   has_many :chore_histories
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
-  validates :name, :street, :apt, :zipcode, presence: true
+  validates :name, :street, :zipcode, presence: true
   validates :zipcode, numericality: { only_integer: true }
   validates :zipcode, length: { is: 5 }
 
