026811924b115a6694115105cdf4d3a9a148cdc7
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-29 16:35:12 -0400

    Created rake tasks for automatically zeroing out total week points and completed week points on user's apt chore assignment day and automatically assigning new chores on apartment chore assignment day

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429151706) do
+ActiveRecord::Schema.define(version: 20140429174218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140429151706) do
     t.string   "default_avatar"
     t.float    "latitute"
     t.float    "longitude"
+    t.string   "chore_assignment_day"
   end
 
   create_table "approvals", force: true do |t|
